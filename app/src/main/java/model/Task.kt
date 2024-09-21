713dfdb63f146a1d97e05437c2abd26a6314bb1e
Sithila69 <kms123sithila@gmail.com>
2024-09-21 22:23:32 +0530

    recycle view

--- app/src/main/java/model/Task.kt
+++ app/src/main/java/model/Task.kt
@@ -2,5 +2,6 @@ data class Task(
     var id: Long,
     var title: String,
     var description: String,
-    var isCompleted: Boolean
+    var isCompleted: Boolean,
+    var elapsedTime: Long = 0L
 )
